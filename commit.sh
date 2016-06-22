e501481bd005d4e6797c969cc768118b0a85d593
root <root@s19204877.onlinehome-server.com>
2016-06-22 22:16:38 +0200

    Bower Setup

---
+++ commit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+read -p "push or pull: " action
+if [ "$action" = "push" ]; then
+read -p "Commit description: " desc
+git add . && \
+git add -u && \
+git commit -m "$desc" && \
+git push origin dev_branch
+elif [ "$action" = "pull" ]; then
+git pull origin dev_branch
+else
+echo invalid input
+fi
