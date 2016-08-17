8e1ae29cedd3342a0b012468cc41a17e4e8db67d
Cody Johnson <cody@anpac.net>
2016-08-17 16:59:46 -0600

    Updated Toolbar

--- app/scripts/services/recruit.js
+++ app/scripts/services/recruit.js
@@ -9,7 +9,7 @@
  */
 angular.module('JFS_Admin')
   .factory('recruit', function ($rootScope,$http) {
-    var currentRecruit = {data:{}};
+    var currentRecruit = {data:{popInfo:{}}};
     currentRecruit.setRecruit = function(id){
       $http({
           method: 'GET',
@@ -26,6 +26,33 @@ angular.module('JFS_Admin')
           console.log(error);
       });
     };
+    currentRecruit.save = function(){
+      $http({
+          method: 'PATCH',
+          url: 'https://jfsapp.com/Secure/API/v2/Recruits/'+  currentRecruit.data.currentRecruit.INDV_ID+'/',
+          params: {
+              'access_token': $rootScope.currentUser.Token.access_token,
+              client_id: 'testclient',
+              client_secret: 'testpass'
+          },
+          data:currentRecruit.data.currentRecruit
+      }).then(function(data) {
+          //console.log(data.data);
+          //currentRecruit.data.currentRecruit = data.data;
+      }, function(error) {
+          console.log(error);
+      });
+    }
+    currentRecruit.popSent =function(){
+      if(!angular.isDefined(currentRecruit.data.currentRecruit.Info.PopStatus)){
+        currentRecruit.data.currentRecruit.Info.PopStatus = {Pops:[],TotalSent:0};
+      }
+      currentRecruit.data.currentRecruit.Info.PopStatus.TotalSent++;
+      currentRecruit.data.currentRecruit.Info.PopStatus.LastSent = moment.utc().format();
+      currentRecruit.data.currentRecruit.POP_Status='Test Sent';
+      currentRecruit.save()
+
+    }
 
     return currentRecruit;
   });
