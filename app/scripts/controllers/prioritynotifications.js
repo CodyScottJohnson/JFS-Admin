956d1594093dcaa934aba4e169ac4c05e07522c6
Cody Johnson <cody@anpac.net>
2016-09-22 08:22:17 -0600

    I did Stuff

--- app/scripts/controllers/prioritynotifications.js
+++ app/scripts/controllers/prioritynotifications.js
@@ -10,4 +10,11 @@
 angular.module('JFS_Admin')
   .controller('PrioritynotificationsCtrl', function ($scope,Notifications) {
     $scope.Notifications = Notifications.data;
+    $scope.PostPone= function(data){
+      var index =  _.findIndex($scope.Notifications.Priority, function(o) { return o.ImportantNotifications_ID == data.ImportantNotifications_ID; });
+      $scope.Notifications.Priority.splice(index,1);
+      if($scope.Notifications.Priority.length===0){
+        $scope.CloseModal();
+      }
+    };
   });
