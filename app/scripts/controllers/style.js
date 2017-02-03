2fcaa3dd9d4e166d8193abe8a28782e439e785d1
Cody Johnson <cody@anpac.net>
2017-02-03 12:57:49 -0700

    something

--- app/scripts/controllers/style.js
+++ app/scripts/controllers/style.js
@@ -8,8 +8,18 @@
  * Controller of the JFS_Admin
  */
 angular.module('JFS_Admin')
-  .controller('StyleCtrl', function ($scope) {
+  .controller('StyleCtrl', function ($scope, $http,FileSaver,Blob,$rootScope) {
     $scope.log=function(a){
       console.log(a);
     };
+    $scope.dowloadLogo = function(name){
+      $rootScope.showLoading=!$rootScope.showLoading;
+      //$rootScope.showLoading=false;
+    $http.get('https://jfsapp.com/Open/API/Images/Brand/Logos/JFS/'+name, {responseType: 'arraybuffer'}).then(function(data){
+      var mime = data.headers()['content-type'];
+      var image = new Blob([data.data],{type:mime});
+      FileSaver.saveAs(image,name);
+      $rootScope.showLoading=!$rootScope.showLoading;
+    })
+    }
   });
